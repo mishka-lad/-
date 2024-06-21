df2af62d10df8b271631882f0280d3c807abb925
mishka-lad <mishka.marochkin.1996@mail.ru>
2024-06-22 04:19:39 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+total_tasks = '12'
+Hours_spent = '1.5'
+course = 'Pathon'
+print(1.5 / 12)
+average_time = '0.125'
+print(course + ':', 'всего задач:' + total_tasks + ',' 'затрачено часов:' + Hours_spent + ','  'среднее время выполнения',average_time, 'часа')
