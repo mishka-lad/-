dd56939191a70ed3495ed7ed2fdee88bca40674b
mishka-lad <mishka.marochkin.1996@mail.ru>
2024-06-23 12:34:52 +1000

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -4,4 +4,4 @@ course = 'Pathon'
 x = Hours_spent / total_tasks
 print(x)
 average_time = x
-print('Курс'+':',course + ','  'всего задач:' + total_tasks + ',' 'затрачено часов:' + Hours_spent + ','  'среднее время выполнения',average_time, 'часа')
+print('Курс' + ':' ,course + ','  'всего задач:', + total_tasks,  'затрачено часов:', Hours_spent,  ',' 'среднее время выполнения', average_time, 'часа')
